b655d34c438b09f6274c6d036cddbebd1dded2b6
Paul Agron <cout.flush@gmail.com>
2016-02-02 15:47:23 -0800

    Initial code commit

---
+++ SampleFramework/SampleFramework/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleFramework
+//
+//  Created by Paul Agron on 2/2/16.
+//  Copyright © 2016 Healthtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ SampleFramework/SampleFramework/MyViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  SampleFramework
+//
+//  Created by Paul Agron on 2/2/16.
+//  Copyright © 2016 Healthtap. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
